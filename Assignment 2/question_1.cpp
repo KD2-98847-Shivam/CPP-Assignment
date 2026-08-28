71dbe760963d594b625378d5a794ba51c48e331f
shivammozar <shivammozar@gmai.com>
2026-08-28 14:44:24 +0530

    Completed assignment

---
+++ Assignment 2/question_1.cpp
@@ -0,0 +1,111 @@
+ /*Q1. Write a menu driven program to calculate volume of the box(length * width * height). 
+Provide parameterless, parameterized(with 3 parameters) and single paramaterized constructor. 
+Create the local objects in respective case and call the function to caluclate area. 
+Menu options ->  
+1. Calculate Volume with default values 
+2. Calculate Volume with length,breadth and height with same value 
+3. Calculate Volume with different length,breadth and height values.*/
+
+#include<iostream>
+using namespace std;
+ 
+
+class Box
+{
+    int length;
+    int width;
+    int height;
+
+    public:
+
+    // parameterless constructor
+    Box()
+    {
+        length = 1;
+        width = 1;
+        height = 1;
+
+    }
+
+    // single parametrised constructor
+    Box(int value)
+    {
+        length = value;
+        width = value;
+        height  = value;
+    }
+
+    // parametrised constructor 
+    Box(int l , int w , int h)
+    {
+        length = l;
+        width = w;
+        height = h;
+    }
+
+    int CalculateVolume()
+    {
+        return length * width * height; // volume calculation
+    }
+
+};
+
+int main()
+{
+    int value;
+    int l , w , h;
+    int choice;
+
+    cout<<"Calculate Volume with default values"<<endl;
+    cout<<"Calculate Volume with length,breadth and height with same value"<<endl;
+    cout<<"Calculate Volume with different length,breadth and height values"<<endl;
+
+    cout<<"Enter your choice:"<<endl;
+    cin>>choice;
+
+    switch (choice)
+    {
+        case 1:
+        {
+            Box b; // object of class Box
+            cout<<"Volume = "<<b.CalculateVolume()<<endl;
+            break; 
+
+        }
+
+         case 2:
+        {
+            
+            cout<<"Enter the value:"<<endl;
+            cin>>value;
+            Box b(value);
+
+            cout<<"Volume ="<<b.CalculateVolume()<<endl;
+            break; 
+
+        }
+
+        case 3:
+        {
+            cout<<"Enter length l:"<<endl;
+            cin>>l;
+
+            cout<<"Enter length w:"<<endl;
+            cin>>w;
+
+            cout<<"Enter length h:"<<endl;
+            cin>>h;
+
+            Box b(l ,w ,h);
+            cout<<"Volume ="<<b.CalculateVolume()<<endl;
+            break;
+
+        }
+
+            default:
+            cout<<"Invalid Choice:"<<endl;
+    }
+
+      return 0;
+
+}
---
+++ Assignment 2/question_2.cpp
@@ -0,0 +1,97 @@
+/* Q2. Imagine a tollbooth at a bridge. Cars passing by the booth are expected to pay a fifty-cent toll.
+Mostly they do, but sometimes a car goes by without paying.
+The tollbooth keeps track of the number of cars that have gone by and of the total amount of money
+collected. Model this tollbooth with a class called tollbooth.
+The two data items are a type unsigned int to hold the total number of cars, and a type double to
+hold the total amount of money collected.
+A constructor initializes all data members to 0. A member function called payingCar() increments
+the car total and adds 0.50 to the cash total. An other function, called nopayCar()
+increments the car total but adds nothing to the cash total. Finally, a member function called
+printOnConsole() displays the two totals and number of paying as well as non paying cars total. */
+ #include<iostream>
+using namespace std;
+
+class tollbooth
+{
+    unsigned int totalCars;
+    double totalCash;
+
+public:
+
+    // Constructor
+    tollbooth()
+    {
+        totalCars = 0;
+        totalCash = 0;
+    }
+
+    // Function for paying car
+    void payingCar()
+    {
+        totalCars++;
+        totalCash = totalCash + 0.50;
+    }
+
+    // Function for non-paying car
+    void nopayCar()
+    {
+        totalCars++;
+    }
+
+    // Function to display totals
+    void printOnConsole()
+    {
+        int payingCars;
+        int nonPayingCars;
+
+        payingCars = totalCash / 0.50;
+        nonPayingCars = totalCars - payingCars;
+
+        cout << "Total Cars = " << totalCars << endl;
+        cout << "Total Cash = " << totalCash << endl;
+        cout << "Paying Cars = " << payingCars << endl;
+        cout << "Non-Paying Cars = " << nonPayingCars << endl;
+    }
+};
+
+int main()
+{
+    tollbooth t;
+    int choice;
+
+    do
+    {
+        cout << "\n1. Paying Car" << endl;
+        cout << "2. Non-Paying Car" << endl;
+        cout << "3. Display Total" << endl;
+        cout << "4. Exit" << endl;
+
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch(choice)
+        {
+            case 1:
+                t.payingCar();
+                break;
+
+            case 2:
+                t.nopayCar();
+                break;
+
+            case 3:
+                t.printOnConsole();
+                break;
+
+            case 4:
+                cout << "Program ended." << endl;
+                break;
+
+            default:
+                cout << "Invalid choice!" << endl;
+        }
+
+    } while(choice != 4);
+
+    return 0;
+}
