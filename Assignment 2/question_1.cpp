4174d79b582f4efb821732f0d0abd86ac758462f
shivammozar <161678117+shivammozar@users.noreply.github.com>
2026-08-29 16:26:20 +0530

    Assignment Complete

--- Assignment 2/question_1.cpp
+++ Assignment 2/question_1.cpp
@@ -1,7 +1,7 @@
  /*Q1. Write a menu driven program to calculate volume of the box(length * width * height). 
 Provide parameterless, parameterized(with 3 parameters) and single paramaterized constructor. 
 Create the local objects in respective case and call the function to caluclate area. 
-Menu options ->  
+Menu options->  
 1. Calculate Volume with default values 
 2. Calculate Volume with length,breadth and height with same value 
 3. Calculate Volume with different length,breadth and height values.*/
